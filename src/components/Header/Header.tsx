6bb62f493248ab402a2af6a8a6eb2064515e6fdd
THARUSHA <tharushadilmin123@gmail.com>
2026-06-19 20:14:21 +0530

    create nav bar

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,8 +1,41 @@
-
+import logo from "../../assets/logo.svg"
 
 const Header = () => {
   return (
-    <div className="w-[80%] h-28 bg-">
+    <div className="fixed top-5 left-1/2 -translate-x-1/2 
+            w-[90%] max-w-6xl
+            bg-white/5
+            backdrop-blur-md
+            border border-white/10
+            rounded-2xl
+            shadow-lg
+            px-6 py-2 mt-4">
+
+        <div className="flex items-center justify-between">
+        
+        
+        <div className=" w-22.5">
+            <img src={logo} alt="logo"></img>
+        </div>
+
+        
+        <div className="flex items-center gap-8">
+            <a href="#" className="relative text-gray-300 hover:text-white after:absolute after:left-0 after:-bottom-1 after:h-0.5 after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full">
+              Home
+            </a>
+            <a href="#" className="relative text-gray-300 hover:text-white after:absolute after:left-0 after:-bottom-1 after:h-0.5 after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full">
+              About
+            </a>
+            <a href="#" className="relative text-gray-300 hover:text-white after:absolute after:left-0 after:-bottom-1 after:h-0.5 after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full">
+              Portfolio
+            </a>
+            <a href="#" className="relative text-gray-300 hover:text-white after:absolute after:left-0 after:-bottom-1 after:h-0.5 after:w-0 after:bg-white after:transition-all after:duration-300 hover:after:w-full">
+              Contact
+            </a>
+
+        </div>
+
+    </div>
       
     </div>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,20 +3,20 @@ import Header from "../components/Header/Header"
 
 const Home = () => {
   return (
-    <div className="w-full h-screen bg-[#120F17]">
+    <div className="w-full h-screen bg-[#120F17] flex justify-center">
+      <Header />
       <ShapeGrid 
         speed={0.1}
-        squareSize={40}
-        direction='diagonal' // up, down, left, right, diagonal
+        squareSize={50}
+        direction='up' // up, down, left, right, diagonal
         borderColor="#2F293A"
         hoverFillColor='#222'
-        shape='square' // square, hexagon, circle, triangle
+        shape='hexagon' // square, hexagon, circle, triangle
         hoverTrailAmount={8} // number of trailing hovered shapes (0 = no trail)
+
       />
-      <Header />
-
-
-</div>
+      
+    </div>
   )
 }
 
