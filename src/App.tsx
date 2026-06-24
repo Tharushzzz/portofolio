e4fc31514d0a2e3bfb9f14b69d60bd9549454f07
THARUSHA <tharushadilmin123@gmail.com>
2026-06-24 14:04:52 +0530

    add page loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
+import { useEffect, useState } from "react";
 import Home from './pages/Home'
 import './App.css'
 import About from './pages/About'
 import Showcase from './pages/Showcase'
-import Footer from './pages/footer'
+import Footer from './pages/Footer'
+import PageLoader from './components/PageLoader'
 
 function App() {
 
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+
   return (
     <>
+
+      {isLoading && <PageLoader />}
+
       <Home />
       <About />
       <Showcase />
